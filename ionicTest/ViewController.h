432e64beab5a9a66707401d72240f6e09151f89b
hillyoung <389181073@qq.com>
2017-12-06 11:01:30 +0800

    提交实例代码

---
+++ ionicTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ionicTest
+//
+//  Created by duck on 2017/12/4.
+//  Copyright © 2017年 duck. All rights reserved.
+//
+
+#import <Cordova/CDVViewController.h>
+
+@interface ViewController : CDVViewController
+
+
+@end
+
